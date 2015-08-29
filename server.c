8290f53bd643c3ec76d47aa8c3eeb2687d7c8cc5
hoenirvili <hoenirvili@gmail.com>
2015-08-29 19:10:57 +0300

    testing select udp sock

--- server.c
+++ server.c
@@ -1,51 +1,72 @@
 #include "lib/net.h"
-int sockfd;
-static void recvfrom_int(int);
-static int count;
-void dg_echo(int sockfd, SA *client, socklen_t clilen);
-
-int main(void)
+int			listenfd, connfd, udpfd, nready, maxfdp1;
+char		mesg[MAXLINE];
+pid_t		childpid;
+fd_set		rset;
+ssize_t		n;
+socklen_t   len;
+const int on = 1;
+void sig_chld(int);
+int
+main(void)
 {
-	sockfd = Socket(AF_INET, SOCK_DGRAM, 0);
-
+	// create listening socket TCP
+	listenfd = Socket(AF_INET,SOCK_STREAM,0);
+	//bind the struct sockaddr with 0
 	memset(&server4_address,0,sizeof(server4_address));
-
 	server4_address.sin_family = AF_INET;
-	server4_address.sin_addr.s_addr = htonl(INADDR_ANY);
 	server4_address.sin_port = htons(PORT);
+	server4_address.sin_addr.s_addr = htonl(INADDR_ANY);
 
+	Setsockopt(listenfd,SOL_SOCKET, SO_REUSEADDR, &on, sizeof(on));
 
-	Bind(sockfd, (struct sockaddr *)&server4_address,sizeof(server4_address));
+	Bind(listenfd, (SA*)&server4_address,sizeof(server4_address));
+	Listen(listenfd,LISTENQ);
 
-	dg_echo(sockfd, (SA*)&client4_address,sizeof(client4_address));
+	/*create udp socket*/
+	udpfd = Socket(AF_INET,SOCK_DGRAM,0);
+	memset(&server4_address,0,sizeof(server4_address));
+	server4_address.sin_family = AF_INET;
+	server4_address.sin_port = htons(PORT);
+	server4_address.sin_addr.s_addr = htonl(INADDR_ANY);
 
-}
+	Bind(udpfd,(SA*)&server4_address,sizeof(server4_address));
 
-
-void dg_echo(int sockfd, SA *client, socklen_t clilen)
-{
-	socklen_t		len;
-	char			mesg[MAXLINE];
-	int				n;
-
-
-	Signal(SIGINT, recvfrom_int);
-
-	n = 220 * 1024;
-	Setsockopt(sockfd, SOL_SOCKET,SO_RCVBUF, &n, sizeof(n));
-
-
-	for(;;)
+	Signal(SIGCHLD,handler_child_wait);
+	FD_ZERO(&rset);
+	maxfdp1 = max(listenfd,udpfd) + 1;
+	for(; ;)
 	{
-		len = clilen;
-		Recvfrom(sockfd, mesg, MAXLINE, 0,client, &len);
-		count ++;
-	}
-}
+		FD_SET(listenfd,&rset);
+		FD_SET(udpfd, &rset);
 
-static void
-recvfrom_int(int signo)
-{
-	printf("\nrecived %d datagrams\n",count);
-	exit(EXIT_SUCCESS);
+		if( (nready = select(maxfdp1, &rset, NULL,NULL, NULL)) <0 )
+			if(errno == EINTR)
+				continue;
+			else
+				prog_error("select error",true,errno);
+		
+		
+		if(FD_ISSET(listenfd, &rset))
+		{
+			len = sizeof(client4_address);
+			connfd = Accept(listenfd, (SA*)&client4_address, &len);
+
+			if(( childpid = Fork() == 0) // child process
+			{
+				Close(listenfd);
+				str_echo(connfd);
+				exit(0);
+			}
+			else
+				Close(connfd);
+		}
+		if(FD_ISSET(udpfd, &rset))
+		{
+			len = sizeof(client4_address);
+			n = Recvfrom(udpfd, mesg, MAXLINE, 0, (SA *)& client4_address, &len);
+
+			Sendto(udpfd,mesg, n, 0, (SA*)&client4_address, len);
+		}
+	}
 }
