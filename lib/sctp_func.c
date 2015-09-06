f3c043746cbd5db9c9a7c45ed2be35f6291958b7
hoenirvili <hoenirvili@gmail.com>
2015-09-06 16:13:45 +0300

    get number of streams function  BSD compatible added

--- lib/sctp_func.c
+++ lib/sctp_func.c
@@ -14,8 +14,23 @@ sctp_address_to_associd(int sockfd, struct sockaddr *sa, socklen_t salen)
 	 return sp.spp_assoc_id;
 }
 //function updated to work with linux
+
+int sctp_get_number_streams_bsd(int sockfd, struct sockaddr *to, 
+								socklen_t tolen)
+{
+	 socklen_t stlen;
+	 struct sctp_status status;
+	 stlen = sizeof(status);
+	 memset(&status, 0, stlen);
+	 status.sstat_assoc_id = sctp_address_to_associd(sockfd,to,tolen);
+	 Getsockopt(sockfd, IPPROTO_SCTP, SCTP_STATUS, &status, &stlen);
+
+	 return status.sstat_outstrms;
+}
+
 int
-sctp_get_number_streams(int sockfd, struct sockaddr *to, socklen_t tolen, struct sctp_sndrcvinfo *sri)
+sctp_get_number_streams(int sockfd, struct sockaddr *to, socklen_t tolen, 
+						struct sctp_sndrcvinfo *sri)
 {
 	 socklen_t stlen;
 	 struct sctp_status status;
